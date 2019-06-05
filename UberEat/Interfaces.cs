a96595e70e684921827371b22b7452fdecdc701a
zrysnd <zrysnd@von-humboldt-25.dynamic.rpi.edu>
2019-06-05 00:13:23 -0400

    class and interfaces added

---
+++ UberEat/Interfaces.cs
@@ -0,0 +1,99 @@
+﻿using System;
+namespace UberEat
+{   
+    /* Reuse: any payment, in any currency*/
+    public interface IPayment
+    {
+
+    }
+
+    /*Reuse: any location, can be address, or a temporary location of an object.*/
+    public interface ILocation
+    {
+
+    }
+
+    /*Reuse: IPurchasable can be any good or services(ex: booking hotel, goods from grocery stores..) that can be purchased */
+    public interface IPurchasable
+    {
+        IPayment Price { get; }
+        IBusinessProvider OrderProvider { get; }
+    }
+
+    /*Reuse: an order containing one or more than one 'any kind of good or services 
+      from the same seller. */
+    public interface IOrder
+    {
+        void AddPurchased(IPurchasable ToBePurchased);
+        IPayment Price { get; }
+        IBusinessProvider OrderProvider { get; }
+    }
+
+    /* Reuse: IOrderPlacable represents any client ordering any good or services */
+    public interface IOrderPlacable
+    {
+        void PlaceOrder(IOrder order, IOrderReceivable orderReceiver);
+    }
+
+    /*Reuse: Any organization, individual providing good or services clients require*/
+    public interface IOrderReceivable
+    {
+        bool OrderAccepted(IOrder order);
+    }
+
+    /* Reuse: IOrderPlacable represents any client paying for any good or services */
+    public interface IPayable
+    {
+        void PayForOrder(IOrder order, IPaymentRecievable seller);
+    }
+
+    /* Reused: any good or service provider receiving payment */
+    public interface IPaymentRecievable
+    {
+        void AcceptPayment(IPayment payment);
+    }
+
+
+    /* Reuse: Location can be address or current location of any moving object*/
+    public interface ILocationProvidable
+    {
+        ILocation Location { get; }
+    }
+
+    /* Reuse: Any goods that can be shipped */
+    public interface IShippable
+    {
+        /*Should I create another interface that's responsible to ship stuffs?*/
+        ILocation Location { get; }
+        //void ShipTo(ILocation target); commented out, I don't think I need it, but not sure.
+    }
+
+    /* Reuse: Any group of shippable goods from a same seller */
+    public interface IShippableOrder: IOrder, IShippable
+    {
+
+    }
+
+    /* Reuse: any client buying goods, and need the goods to be delivered. */
+    public interface IClient: IOrderPlacable, IPayable, ILocationProvidable
+    {
+
+    }
+
+    /*  Reuse: any good provider that need to deliver goods to client*/
+    public interface IBusinessProvider: IOrderReceivable, IPaymentRecievable, ILocationProvidable
+    {
+
+        void AskProviderToDeliverOrderedGoods(IShippable order, IClient client );
+    }
+
+    /* Reuse: A collection of good or service providers that can be displayed on a software,
+     they provide some type of good or service: food, booking rooms, online groceries etc.  */
+    public interface IAvaibleBusinessProviders
+    {
+        void UpdateAvailableProviders();
+        void DisplayProviders();
+    }
+
+
+}
---
+++ UberEat/UberEat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace UberEat
+{
+    public class UberEat
+    {
+        IClient _Client;
+        IAvaibleBusinessProviders _AvailableRestaurants;
+        IBusinessProvider _RestaurantSelected;
+        IShippableOrder _Order;
+
+        public UberEat()
+        {   
+            //_Client = logged in user //Do I need to show how to implemetn this?..
+            _AvailableRestaurants.UpdateAvailableProviders();
+        }
+
+        public void SelectRestaurant(IBusinessProvider RestaurantSelected)
+        {
+            _RestaurantSelected = RestaurantSelected;
+        }
+
+        public void AddFoodToOrder(IPurchasable food  )
+        {
+            _Order.AddPurchased(food);
+        }
+
+        public bool HandleOrder()
+        {
+            /*Not sure about whether I need this line or not, I think the software need to
+            wait for restaurant to accept order, assuming the frontend calls this
+            HandleOrder() in a while loop or other possible proper ways. */
+            if (!_RestaurantSelected.OrderAccepted(_Order))
+                return false;
+            _Client.PayForOrder(_Order, _RestaurantSelected);
+            _RestaurantSelected.AskProviderToDeliverOrderedGoods(_Order, _Client);
+            return true;
+        }
+    }
+}
