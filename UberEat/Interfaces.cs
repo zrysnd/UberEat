ddaf580b1f68ae72a40fa422e66de2d4f807e5d1
XHBLX <37951357+XHBLX@users.noreply.github.com>
2019-06-07 17:02:07 -0700

    Update Interfaces.cs

--- UberEat/Interfaces.cs
+++ UberEat/Interfaces.cs
@@ -98,6 +98,8 @@ namespace UberEat
     }
 
 
+    // your file name is Interfaces.cs, why am I seeing things below that are not Interfaces????
+    
     /*classes needed for main function test */
     public class Client : IClient
     {
